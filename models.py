914397c20edb4da7c4d2854199d45d63167722f8
woodsgao <woodsgao@outlook.com>
2019-12-09 15:07:26 +0800

    use resnet by default

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from utils.modules.nn import CNS
-from utils.modules.backbones import BasicModel, EfficientNet
+from utils.modules.backbones import BasicModel, EfficientNet, ResNet
 
 
 # gap classification model
@@ -9,11 +9,12 @@ class EfficientNetGCM(BasicModel):
     def __init__(self, num_classes, model_id=0):
         super(EfficientNetGCM, self).__init__()
         # full pre-activation
-        self.backbone = EfficientNet(model_id)
+        # self.backbone = EfficientNet(model_id)
+        self.backbone = ResNet()
         self.fc = nn.Sequential(
-            CNS(self.backbone.out_channels[-1], self.backbone.out_channels[-1] * 4, 1),
+            # CNS(self.backbone.out_channels[-1], self.backbone.out_channels[-1] * 4, 1),
             nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(self.backbone.out_channels[-1] * 4, num_classes, 1),
+            nn.Conv2d(1024, num_classes, 1),
         )
         self.init()
         self.num_classes = num_classes
