963cb5bc9e59bd0d55ded8b48d20be07acc41a0b
woodsgao <woodsgao@outlook.com>
2019-12-31 09:32:44 +0800

    update

--- export2onnx.py
+++ export2onnx.py
@@ -1,13 +1,12 @@
 import torch
-from utils.models import EfficientNet
+from pytorch_modules.backbones.resnet import resnet18
 import cv2
 
 dummy_input = torch.rand([1, 3, 64, 64])
-model = EfficientNet(20)
-weights = torch.load('best.pt', map_location='cpu')['model']
+model = resnet18(num_classes=3)
+weights = torch.load('weights/plate.pt', map_location='cpu')['model']
 model.load_state_dict(weights)
 model.eval()
-model.fuse()
 torch.onnx.export(model, dummy_input, 'best.onnx')
 net = cv2.dnn.readNetFromONNX('best.onnx')
 net.setInput(dummy_input.numpy())
